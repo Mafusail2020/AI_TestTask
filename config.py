0b2b330c6b30543209f12c53df2afdda5552599b
Mafusail2020 <krutkev00@gmail.com>
2026-02-28 01:05:49 +0200

    Update config

--- config.py
+++ config.py
@@ -3,11 +3,10 @@ from os import getenv
 from pydantic import BaseModel
 
 load_dotenv()
-# USE IN CASE YOU HAVE OPENAI KEY
 
 class Config(BaseModel):
-    API_KEY: str
+    TOKEN: str # Bot token for telegram
 
 config = Config(
-    API_KEY=getenv("OPENAI_API_KEY")
+    TOKEN=getenv("TOKEN")
 )
